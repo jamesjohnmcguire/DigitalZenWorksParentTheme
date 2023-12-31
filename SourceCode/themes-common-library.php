cf9f0abf8b7470c949fb08c46cce132f78442a51
James John McGuire <jamesjohnmcguire@gmail.com>
2023-12-31 17:33:29 +0900

    Remove Duplicate Functions

--- SourceCode/themes-common-library.php
+++ SourceCode/themes-common-library.php
@@ -3,41 +3,6 @@
 Common functions used across different WordPress themes.
 */
 
-function deregister_styles()
-{
-	if (!is_admin_bar_showing())
-	{
-		wp_deregister_style('open-sans');
-	}
-}
-
-function disable_emojicons_tinymce( $plugins )
-{
-	if ( is_array( $plugins ) )
-	{
-		return array_diff( $plugins, array( 'wpemoji' ) );
-	}
-	else
-	{
-		return array();
-	}
-}
-
-function disable_wp_emojicons()
-{
-	// all actions related to emojis
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-
-	// filter to remove TinyMCE emojis
-	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-}
-
 function get_archive_title()
 {
 	$message = null;
