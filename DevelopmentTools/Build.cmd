8a5e608ce6a020cc777895e50962a48a5d0c38cd
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-14 21:33:13 +0900

    Add Build Script

---
+++ DevelopmentTools/Build.cmd
@@ -0,0 +1,33 @@
+CD %~dp0
+CD ..
+CD SourceCode\themes\digitalzenworks
+
+CALL composer validate --strict
+CALL composer install --prefer-dist
+ECHO outdated:
+CALL composer outdated
+
+ECHO Checking code styles...
+php vendor\bin\phpcs -sp --standard=ruleset.xml .
+
+@ECHO ON
+CD SourceCode\themes\digitalzenworks-irdi
+IF EXIST vendor.bak\NUL RD /S /Q vendor.bak
+IF EXIST vendor.production\NUL RD /S /Q vendor.production
+
+ECHO Backing up vendor
+XCOPY /C /D /E /H /I /R /S /Y vendor vendor.bak >NUL
+
+ECHO Installing vendor no dev
+CALL composer install --no-dev --prefer-dist
+@ECHO ON
+
+CD vendor
+
+DEL /S /Q *.md >NUL
+CD ..
+
+REN vendor vendor.production
+REN vendor.bak vendor
+
+:finish
