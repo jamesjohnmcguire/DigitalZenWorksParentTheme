b7aa15e5e3d5b2dff5cffe181e26e240fa72a7b0
James John McGuire <jamesjohnmcguire@gmail.com>
2024-05-28 15:12:16 +0900

    Update Meta tags

--- SourceCode/functions.php
+++ SourceCode/functions.php
@@ -605,7 +605,9 @@ if (!function_exists('\DigitalZenWorksTheme\output_head'))
 ?>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
     <title><?php echo $title; ?></title>
 <?php
 		if ($icon_file != null)
