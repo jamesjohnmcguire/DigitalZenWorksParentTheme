d562b476669e7d4444f89a774b77e21f3ae2b62b
James John McGuire <jamesjohnmcguire@gmail.com>
2024-07-21 20:42:41 +0900

    Clean Up Code

--- SourceCode/front-page.php
+++ SourceCode/front-page.php
@@ -6,9 +6,9 @@ Template Name: Front Page
 declare(strict_types=1);
 
 namespace DigitalZenWorksTheme;
- 
+
 get_header();
- ?>
+?>
     <!-- front-page.php -->
 <?php
 if (is_home())
@@ -23,20 +23,18 @@ if (is_front_page())
     <!--is_front_page-->
 <?php
 }
-?>
 
-<?php
-	if ((is_home()) && (is_front_page()))
-	{
-		// show a list of posts
-		echo "<!--home-->\r\n";
-		// in functions.php
-		get_the_posts();
-	}
-	else
-	{
-		// show our main default page
-		the_content();
-	}
+if ((is_home()) && (is_front_page()))
+{
+	// show a list of posts
+	echo "<!--home-->\r\n";
+	// in functions.php
+	get_the_posts();
+}
+else
+{
+	// show our main default page
+	the_content();
+}
 
 get_footer();
