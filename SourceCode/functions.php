297987493ea3e9b818f13839d996653569a887b1
James John McGuire <jamesjohnmcguire@gmail.com>
2024-11-02 15:31:34 +0900

    Add Potenial Hooks

--- SourceCode/functions.php
+++ SourceCode/functions.php
@@ -16,12 +16,19 @@ defined('THEME_DEBUG') OR define('THEME_DEBUG', false);
 // Remove the Link header for the WP REST API, as this (falsely) causes
 // W3C validation errors
 add_action('after_setup_theme', '\DigitalZenWorksTheme\remove_head_rest');
+// add_action('after_setup_theme', 'register_primary_menu');
 
 // admin - customize them
 add_action('customize_register', '\DigitalZenWorksTheme\theme_customizer');
 add_action('phpmailer_init', '\DigitalZenWorksTheme\mailer_config', 10, 1);
 add_action('wp_enqueue_scripts', '\DigitalZenWorksTheme\dequeue_assets');
 add_action('wp_enqueue_scripts', '\DigitalZenWorksTheme\enqueue_assets');
+//Remove Gutenberg Block Library CSS from loading on the frontend
+// add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
+
+add_filter('show_admin_bar', '__return_false');
+// add_filter('wp_mail_from_name','from_mail_name');
+// add_filter('wp_nav_menu_objects', 'modify_wp_nav_menu_objects', 10, 2);
 
 // add the home link to the main menu, if needed
 add_filter('wp_nav_menu_items', '\DigitalZenWorksTheme\add_home_link', 10, 2);
